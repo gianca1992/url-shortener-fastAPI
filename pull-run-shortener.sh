294dac341c67d37faaee3ff8ed7ff49335f82fce
Giancarlo Patane <72011850+gianca1992@users.noreply.github.com>
2023-02-23 21:29:39 +0000

    Create pull-run-shortener.sh

---
+++ pull-run-shortener.sh
@@ -0,0 +1,16 @@
+
+#!/bin/bash
+
+# install and configure docker on the ec2 instance
+sudo yum update -y
+sudo amazon-linux-extras install docker -y
+sudo service docker start
+sudo systemctl enable docker
+
+# pull the image you previously built and pushed locally (make sure to replace the line with your contents)
+sudo docker pull  your-dockerhub-username/repo-for-shortener
+
+# start the container  
+sudo docker run -d -p  8000:8000 your-dockerhub-username/repo-for-shortener:latest
+
+
